079a59e07de175c4d591c164dd41502c3f88be9c
Myriam Pedroza <11456@utl.edu.mx>
2023-10-13 22:10:57 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Login from './components/Login'
+import Menu from './components/Menu'
 function App() {
   return (
     <div>
-        <Login/>
-
+        <Menu/>
     </div>
   );
 };
