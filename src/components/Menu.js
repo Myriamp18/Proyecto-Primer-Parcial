ad9743b8bf4d83653cec3a8ea10d6f04c4a69751
Myriam Pedroza <11456@utl.edu.mx>
2023-10-14 13:59:46 -0600

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,31 @@
 import React,{useState} from "react";
 import "./Menu.css"
 
+
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false)
     return(
         <div className="navbar">
             <div className="nav_logo">MENU</div>
-            <div className={'nav_items ${isOpen && "open"}'}>
+            <div className={`nav_items ${isOpen ? "open" : ""}`}>
                 <a href="#">INICIO</a>
                 <a href="#">PERFIL</a>
                 <a href="#">CONFIGURACIONES</a>
                 <a href="#">AYUDA</a>
             </div>
-            <div className={'nav_toggle ${isOpen && "open"}'} onClick={ () => setIsOpen(!isOpen) }>
+            <div className={`nav_toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
 
-        </div>
+            </div>
+              
+
+        
     )
+
+    
 }
 
 export default Menu;
