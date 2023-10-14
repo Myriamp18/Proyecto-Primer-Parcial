1c3a5b2354cf84b87fdc484919016710bd7c1d24
Myriam Pedroza <11456@utl.edu.mx>
2023-10-14 13:59:25 -0600

    Create Inicio.js

---
+++ src/components/Inicio.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Inicio = () => {
+    
+    return(
+        <div className="">
+            HOLA
+        </div>
+
+        
+    )
+
+    
+}
+
+export default Inicio;
