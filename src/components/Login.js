7852279d5eb69dc8d52e94329ed7673ea85a70af
Myriam Pedroza <11456@utl.edu.mx>
2023-10-13 20:36:14 -0600

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,34 @@
+import React from 'react'
+import './Login.css'
 
-import {useContext} from 'react';
-import {useMyContext} from '../app/MyStateProvider';
+import email_icon from './Asserts/sobre.png'
+import password_icon from './Asserts/llave.png'
 
-const Login = () =>{
-    const [state,setState] = useMyContext();
-
-    return( <input type="text" onChange={ (e) => {setState({ ...state, name:e.target.value})}} />
+const Login =() => {
+    return (
+        <div className='container'>
+            <div className="header">
+                <div className="text">LOGIN</div>
+                <div className="underline"></div>
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src={email_icon} alt=''/>
+                        <input type="email" placeholder='Correo Electronico'/>
+                    </div>
+                    <div className="input">
+                        <img src={password_icon} alt=''/>
+                        <input type="password" placeholder='Contraseña'/>
+                    </div>
+                </div>
+            
+            <div className="forgot-password"></div>
+            <div className="submit-container">
+                <div className="submit">Iniciar Sesion</div>
+            </div>
+            
+            </div>
     );
-}
+};
 
 export default Login;
