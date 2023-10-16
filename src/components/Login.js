a10e75c90737eec2c1f61f0d1aa61a44a1441517
Myriam Pedroza <11456@utl.edu.mx>
2023-10-15 18:32:19 -0600

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Login.css'
-
+import {useNavigate} from 'react-router-dom'
 import email_icon from './Asserts/sobre.png'
 import password_icon from './Asserts/llave.png'
 
 const Login =() => {
+   const navigate = useNavigate();
+
+    const handleRedireccion = () => {
+        // Para redirigir a otra página, usa la función navigate.
+        navigate('/Menu');
+    };
+
     return (
         <div className='container'>
             <div className="header">
@@ -24,7 +31,7 @@ const Login =() => {
             
             <div className="forgot-password"></div>
             <div className="submit-container">
-                <div className="submit">Iniciar Sesion</div>
+                <div className="submit" onClick={handleRedireccion}>Iniciar Sesion</div>
             </div>
             
             </div>
