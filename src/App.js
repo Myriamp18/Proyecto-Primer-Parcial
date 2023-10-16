28a6b1145c48da1d05eea1eddd1074f736783717
Myriam Pedroza <11456@utl.edu.mx>
2023-10-15 18:31:32 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Login from './components/Login'
 import Menu from './components/Menu'
+import { BrowserRouter, Routes , Route} from 'react-router-dom';
+
 function App() {
   return (
     <div>
-        <Menu/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} /> {/* Ruta de inicio, muestra el componente Login */}
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
