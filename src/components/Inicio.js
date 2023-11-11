97e4ecf56e5a0758ba1dc7136f09611b2bdf6c86
Myriam Pedroza <11456@utl.edu.mx>
2023-11-10 19:31:28 -0600

    Update Inicio.js

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React,{} from "react";
 import "./Inicio.css"
 import IA_1 from './Asserts/IA1.png'
-import {useNavigate} from 'react-router-dom'
+//import {useNavigate} from 'react-router-dom'
+import {Configuration, OpenAIApi} from 'openai';
+
+
+const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_MY_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 
 const Inicio = () => {
-    const navigate = useNavigate();
-    const handleRedireccion = () => {
+    
+async function fetchData(){
+    try{
+      const response = await openai.createImagen({
+        prompt:"cat with wings",
+        n: 1,
+        size:"512x512",
+
+      })
+      console.log(response.data.data[0].url);
+
+    } catch(e){
+        console.log(e);
+    }
+
+}
+
+
+
+
+    //const navigate = useNavigate();
+   // const handleRedireccion = () => {
         // Para redirigir a otra página, usa la función navigate.
-        navigate('/Login');
-    };
+     //   navigate('/');
+   // };
     
     return(
         <div>
         
-        <div className="sub">PHOTOIA</div>
+        <div className="submm">PHOTOIA</div>
         <div className="texto">Esta aplicacion es un tipo de generador de fotografias en IA, El generador de imagenes te permite tener siempre a mano la imagen perfecta, aunque no exista.
         crea imagenes  que visualicen un producto o una idea, esboza un concepto creativo o supera los lomites de lo sposible. solo tienes que escribir un texto. </div>
         <div className="img-1">
@@ -23,7 +52,7 @@ const Inicio = () => {
         <div className="inputs">
         <input type="CONTENEDOR" placeholder='Descripcion de fotografía IA  a generar'/>
         </div>
-        <div className="submit" onClick={handleRedireccion}>GENERAR</div>
+        <div className="submit" onClick={fetchData}>GENERAR</div>
     </div>
     
         
